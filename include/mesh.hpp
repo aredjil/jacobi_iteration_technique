207e92b7afd13b31bfe9d47b720bdd81a8407b6e
bellou <rand@random.it>
2024-10-03 14:08:32 +0200

    fixed the mesh class

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include<iostream>
 #include<vector>
+#include <fstream>
 template <typename T>
 /**
  * This class store th grid used 
@@ -24,33 +25,34 @@ class Mesh{
     /**
      * Class constructor to intialize boundary conditions.
      */
-    Mesh(size_t n);
+    template <typename F>
+    Mesh(size_t n, F boundary_conditions);
     /**
      * Printing the mesh
      */
     template <typename R>
     friend std::ostream& operator<<(std::ostream& os, const Mesh<R>& m);
-    /**
-     * A function that sets the boundary conditons and returns the field.
-     */
 };
 /**
  * Constructor definition to intilize the mesh using the boundary conditions using the function.
  */
 template <typename T>
-Mesh<T>::Mesh(size_t n):size(n), current_field(n , std::vector<T>(n, 0)){
+template <typename F>
+Mesh<T>::Mesh(size_t n, F boundary_conditions):size(n), current_field(n, std::vector<T>(n, 0.5)){
         /**
          * Set the boundary conditions.
          */
-    // current_field(n, std::vector<int>(n, 0))
-    T max_val{100.0}; 
-    T n_steps = size; // Number of steps 
-    T step_size = max_val/n_steps; // Step size
-    for(size_t i = 0; i < size; i++) // Iterate over the rows 
-    {
-        current_field[i][0] = max_val; // Update the value of the field. 
-        max_val -= step_size; // Update the value for next field. 
-    }
+    // // current_field(n, std::vector<int>(n, 0))
+    // T step = 0;
+    // T max_val{100.0}; 
+    // T n_steps = size; // Number of steps 
+    // T step_size = max_val/(n_steps-1); // Step size, -1 to include 100.
+    // for(size_t i = 0; i < size; i++) // Iterate over the rows 
+    // {
+    //     current_field[i][0] = step; // Update the value of the field. 
+    //     step += step_size; // Update the value for next field. 
+    // }
+    boundary_conditions(current_field);
 }
 /**
  * Definition of the print functionality of the mesh calss
@@ -62,10 +64,10 @@ std::ostream& operator<<(std::ostream& os, const Mesh<T>& m){
     {
         for (size_t j = 0; j < m.size; j++)
         {
-            os<<"\t"<<m.current_field[i][j]<<"\t";
+            // os<<" "<< m.current_field[i][j] <<" ";
+            os<<" "<<m.current_field[i][j]<<" ";
         }
         os<<std::endl;
-        
     }
     return os;
 }
