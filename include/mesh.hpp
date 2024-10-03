e3664ab60e918caa67f0957b831bba330ca16d41
bellou <rand@random.it>
2024-10-03 16:17:46 +0200

    flattenned the matrix

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -17,16 +17,16 @@ class Mesh{
     /**
      * Current field at iteration t.
      */
-    std::vector<std::vector<T>> current_field;
+    std::vector<T> current_field;
     /**
      * New field at iteration t+1.
      */
-    std::vector<std::vector<T>> new_field;
+    std::vector<T> new_field;
     /**
      * Class constructor to intialize boundary conditions.
      */
     template <typename F>
-    Mesh(size_t n, F boundary_conditions);
+    Mesh(size_t n, F &boundary_conditions);
     /**
      * Printing the mesh
      */
@@ -38,21 +38,11 @@ class Mesh{
  */
 template <typename T>
 template <typename F>
-Mesh<T>::Mesh(size_t n, F boundary_conditions):size(n), current_field(n, std::vector<T>(n, 0.5)){
+Mesh<T>::Mesh(size_t n, F &boundary_conditions):size(n), current_field(n*n, 0.5){
         /**
          * Set the boundary conditions.
          */
-    // // current_field(n, std::vector<int>(n, 0))
-    // T step = 0;
-    // T max_val{100.0}; 
-    // T n_steps = size; // Number of steps 
-    // T step_size = max_val/(n_steps-1); // Step size, -1 to include 100.
-    // for(size_t i = 0; i < size; i++) // Iterate over the rows 
-    // {
-    //     current_field[i][0] = step; // Update the value of the field. 
-    //     step += step_size; // Update the value for next field. 
-    // }
-    boundary_conditions(current_field);
+            boundary_conditions(current_field);
 }
 /**
  * Definition of the print functionality of the mesh calss
@@ -65,7 +55,7 @@ std::ostream& operator<<(std::ostream& os, const Mesh<T>& m){
         for (size_t j = 0; j < m.size; j++)
         {
             // os<<" "<< m.current_field[i][j] <<" ";
-            os<<" "<<m.current_field[i][j]<<" ";
+            os<<" "<<m.current_field[i*m.size+j]<<" ";
         }
         os<<std::endl;
     }
