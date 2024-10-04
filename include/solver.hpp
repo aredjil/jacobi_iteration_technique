6594d901c063704e5e1decae394de5ef80fdc93c
bellou <rand@random.it>
2024-10-04 08:49:40 +0200

    created print-results function

--- include/solver.hpp
+++ include/solver.hpp
@@ -24,6 +24,11 @@ class Solver{
      * @param const size_t & print_interval: interval for a snapshot of the grid. used to generating the GIF. 
      */
     void jacobi(Mesh<T>& m, const size_t &max_steps, const size_t & print_interval);
+    private: 
+    /**
+     * Print results to a files 
+     */
+    void print_results(Mesh<T> &m, size_t &counter);
 };
 /**
  * The solver
@@ -31,7 +36,7 @@ class Solver{
 template <typename T>
 void Solver<T>::jacobi(Mesh<T>& m, const size_t &max_steps, const size_t & print_interval){
     auto n = m.size;
-    int counter =1;
+    size_t counter =1;
     for (size_t k = 0; k < max_steps; k++) // Iterate over the total number of steps 
     {
         for (size_t i = 1; i < n-1; i++) // Iterate the rows.
@@ -46,8 +51,15 @@ void Solver<T>::jacobi(Mesh<T>& m, const size_t &max_steps, const size_t & print
         // Swap the current field values with the new field values before the next iteration
         m.new_field.swap(m.current_field);        
         if(k % print_interval == 0){
+        print_results(m, counter);
+        }
         
-        const int width = 3;
+
+    }
+}
+template <typename T>
+void Solver<T>::print_results(Mesh<T> &m, size_t &counter){
+    const int width = 3;
         std::ostringstream file_name;
         file_name << "../gif/file_" << std::setw(width) <<std::setfill('0')<< counter << ".dat";
         counter++;
@@ -58,8 +70,4 @@ void Solver<T>::jacobi(Mesh<T>& m, const size_t &max_steps, const size_t & print
         } else {
             std::cerr << "Error creating file: " << file_name.str() << std::endl;
         }
-        }
-        
-
-    }
 }
