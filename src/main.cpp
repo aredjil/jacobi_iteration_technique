e691fe016821114b32e1c175a25f782ecd8b4f56
bellou <rand@random.it>
2024-10-04 08:50:47 +0200

    changed paramters

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include<cmath>
 template <typename T>
 void boundary_conditions(std::vector<T> &field){
-    // current_field(n, std::vector<int>(n, 0))
     auto n = std::sqrt(field.size());
     T step = 0; // Initial step 
     T max_val{100.0}; // maximum value of the oundary condditions
@@ -30,8 +29,8 @@ void boundary_conditions(std::vector<T> &field){
 
 int main(){
     int n = 100;
-    int max_steps = 10000;  
-    int print_interval = 10;
+    int max_steps = 1000;  
+    int print_interval = 5;
     Mesh<float> mesh(n, boundary_conditions<float>);
     Solver<float> solver;
     solver.jacobi(mesh, max_steps, print_interval);
